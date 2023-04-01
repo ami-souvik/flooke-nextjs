2add06dff255bef52bc9d792779d48b84091a53f
Sav <dsouvik141@gmail.com>
2023-04-02 03:56:12 +0530

    INIT

--- app/orders/page.js
+++
@@ -1,9 +0,0 @@
-"use client"
-import { useContext } from 'react'
-import { FirebaseRealtimeDB } from '../../context/context'
-
-export default function Orders() {
-  const { edibles, orders } = useContext(FirebaseRealtimeDB);
-  return (
-  );
-}
